6057b6a089ea7c9361a44ce286bbdd4d71398520
Michael Mancha <mancham@hawaii.edu>
2018-02-13 13:03:29 -1000

    Finished assignment 4
    Learned arrays and array literals

--- PlaygroundsThree.playground/Contents.swift
+++ PlaygroundsThree.playground/Contents.swift
@@ -3,3 +3,12 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var fruitNames: [String]
+fruitNames = ["Orange", "Apples", "Watermelon"]
+print(fruitNames)
+
+for name in fruitNames {
+    print("I like to eat" + name)
+}
+print(fruitNames[0])
